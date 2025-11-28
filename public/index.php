48344f484e724942e01161b1ddc769d3e8d5b2c1
efffii19 <iamirfan200@gmail.com>
2025-11-28 14:47:23 +0500

    made it production ready

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once __DIR__ . '/../vendor/autoload.php';
+
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
 session_start();
 
 use eftec\bladeone\BladeOne;
